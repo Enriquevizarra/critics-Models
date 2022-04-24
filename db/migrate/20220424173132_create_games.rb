f3b9e696ff7bd2227b3b50d8d99e361f58041cb2
Enrique Vizarra <vizarramattay@gmail.com>
2022-04-24 12:37:50 -0500

    ADD: model Game

---
+++ db/migrate/20220424173132_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration[7.0]
+  def change
+    create_table :games do |t|
+      t.string :name
+      t.string :genre
+      t.string :price
+      t.string :release_date
+
+      t.timestamps
+    end
+  end
+end
